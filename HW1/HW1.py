cee49f90d295f321acf240999036066cf25b20ba
Your Name <youremail@domain.com>
2021-01-28 23:40:19 +0100

    Part 1 is solved basically

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,3 +1,23 @@
-with open('SampleCoordinates.txt', 'r') as file:
-    for line in file:
-        x = print(line, end='')
+"""
+SampleCoordinates.txt
+GermanyCities.txt
+
+"""
+"Indata"
+
+R = 1
+import numpy as np
+import re
+with open('SampleCoordinates.txt', mode='r') as file:
+    text = file.read()
+resultt = [float(d) for d in re.findall('-?\d+\.?\d*',text)]
+#result = np.array(resultt)
+x = []
+y = []
+for i in range(1, len(resultt), 2):
+    x = x + [resultt[i]]
+    y = y + [resultt[i-1]]
+x1 = np.array(x)
+y1 = np.array(y)
+x1 = x * (R * np.pi / 180)
+y1 = R * np.ln(np.tan((np.pi/4)+(np.pi*y/360)))
